cecc9ed3dd9add592ed986bc971c8e4d40559561
Mohammad Shojaei <shojaeireal@gmail.com>
2024-05-30 04:40:06 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import fitz  # PyMuPDF
 import g4f
+import openai
+import groq
 
-def translate_text(text, language):
+def translate_text(text, language, prov, model, key):
     prompt = f"""Translate the following text into Native {language},
     ensuring that you fully comprehend the context and accurately convey the intended meanings.
     Special names or technical terms, and paragraphs should remain Untouch.
@@ -14,25 +16,47 @@ def translate_text(text, language):
     \n
     **Do not add any additional notes**
     """
-    providers = [
-        g4f.Provider.Bing, g4f.Provider.Aichatos,
-        g4f.Provider.GeekGpt, g4f.Provider.Liaobots,
-        g4f.Provider.Phind, g4f.Provider.HuggingChat
-    ]
-    for provider in providers:
-        try:
-            response = g4f.ChatCompletion.create(
-                model=g4f.models.default,
-                provider=provider,
-                messages=[{"role": "system", "content": "you are a professional translator"},
-                          {"role": "user", "content": prompt}],
-            )
-            translated_text = response
-            return translated_text
-        except Exception as e:
-            print(f"Translation failed with provider {provider}: {e}")
-            continue
-    return "Translation failed"
+    if prov == 'Free (Slow)':
+        providers = [
+            g4f.Provider.Bing, g4f.Provider.Aichatos,
+            g4f.Provider.GeekGpt, g4f.Provider.Liaobots,
+            g4f.Provider.Phind, g4f.Provider.HuggingChat
+        ]
+        for provider in providers:
+            try:
+                response = g4f.ChatCompletion.create(
+                    model=g4f.models.default,
+                    provider=provider,
+                    messages=[{"role": "system", "content": "you are a professional translator"},
+                            {"role": "user", "content": prompt}],
+                )
+                return response
+            except Exception as e:
+                print(f"Translation failed with provider {provider}: {e}")
+                continue
+            
+    elif prov == 'OpenAI':
+        client = openai.OpenAI(api_key=key)
+        completion = client.chat.completions.create(
+            model= model,
+            messages=[
+                {"role": "system", "content": "you are a professional translator"},
+                {"role": "user", "content": prompt}
+            ]
+        )
+        return completion.choices[0].message.content
+    elif prov == 'Groq':
+        client = groq.Groq(api_key=key)
+        completion = client.chat.completions.create(
+            model= model,
+            messages=[
+                {"role": "system", "content": "you are a professional translator"},
+                {"role": "user", "content": prompt}
+            ]
+        )
+        return completion.choices[0].message.content
+    
+    
 
 def extract_text_by_paragraph(uploaded_file):
     try:
@@ -57,6 +81,14 @@ translated_texts = []
 paragraphs_text = ''
 
 with st.sidebar:
+    prov = st.selectbox('Choose Translation Provider:', ('Free (Slow)', 'OpenAI', 'Groq'))
+    if prov == 'OpenAI':
+        key = st.text_input("Enter Your API key", type="password")
+        model =  st.selectbox('Choose LLM Model:', ('gpt-3.5-turbo', 'gpt-4o', 'gpt-4-turbo','gpt-4'))
+    if prov == 'Groq':
+        key = st.text_input("Enter Your API key", type="password")
+        model =  st.selectbox('Choose LLM Model:', ('llama3-70b-8192', 'mixtral-8x7b-32768', 'gemma-7b-it'))
+    
     uploaded_file = st.file_uploader("Choose a file")
     if uploaded_file:
         paragraphs_text = extract_text_by_paragraph(uploaded_file)
@@ -75,7 +107,7 @@ if paragraphs_text:
         my_bar = st.progress(0, text=progress_text)
         
         for i, text in enumerate(paragraphs_text):
-            t_text = translate_text(text, language)
+            t_text = translate_text(text, language , prov , model, key)
             translated_texts.extend(t_text)
             my_bar.progress((i + 1)/paragraph_count, text=progress_text)
             if language == "Persian":
@@ -84,8 +116,3 @@ if paragraphs_text:
                 st.markdown(t_text)
                 
         st.download_button("Download Translation", "".join(translated_texts) )
-        
-              
-
-
-            
